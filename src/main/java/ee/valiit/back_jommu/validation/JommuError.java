4e79a6f6117e9bacd6d98683fe57d0307ccd4d13
Mai-Triin <110418700+Mai-Triin@users.noreply.github.com>
2022-11-22 22:55:44 +0200

    develop - changed BankError to JommuError

--- src/main/java/ee/valiit/back_jommu/validation/JommuError.java
+++ src/main/java/ee/valiit/back_jommu/validation/JommuError.java
@@ -3,13 +3,13 @@ package ee.valiit.back_jommu.validation;
 import lombok.Getter;
 
 @Getter
-public enum BankError {
+public enum JommuError {
     INCORRECT_CREDENTIALS("Sellist kasutajanime või parooli ei leitud", "666");
 
     private String message;
     private String errorCode;
 
-    BankError(String message, String errorCode) {
+    JommuError(String message, String errorCode) {
         this.message = message;
         this.errorCode = errorCode;
     }
