0037d5b1ba7dee1fd6d49cf10c5b39c470f0b461
Mihke <mihkelviirsalu@hotmail.com>
2022-11-28 14:41:35 +0200

    JOM-5 - Commit

--- jommuDb/jommu_data.sql
+++ jommuDb/jommu_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.role (id, type) VALUES (DEFAULT, 'Treener');
-INSERT INTO public.role (id, type) VALUES (DEFAULT, 'Treenitav');
+INSERT INTO public.role (id, type) VALUES (DEFAULT, 'Treenija');
 
-INSERT INTO public."user" (id, username, password, role_id) VALUES (DEFAULT, 'treener123', '123', 1);
-INSERT INTO public."user" (id, username, password, role_id) VALUES (DEFAULT, 'treenitav123', '456', 2);
+INSERT INTO public."user" (id, username, password, role_id) VALUES (DEFAULT, 'treener', '123', 1);
+INSERT INTO public."user" (id, username, password, role_id) VALUES (DEFAULT, 'treenija', '123', 2);
